6f7a71f4eed82b77da1d12ebf837a881b0665b2c
Alex Tumanov <uwontrememberthis@gmail.com>
2017-07-23 19:56:44 +0300

    gitignore exclude ./idea

---
+++ src/constructors.js
@@ -0,0 +1,11 @@
+export const c = {
+  users: (n) => {
+    const arr = []
+
+    for (let i = 0; i < n; ++i) {
+      arr.push(i)
+    }
+
+    return arr
+  },
+}
--- src/first.js
+++ src/first.js
@@ -1,1 +1,39 @@
-console.log('i can change')
+const c = require('./constructors')['c']
+
+const n = c.users(10)
+const treesWeight = []
+
+const getRoot = (id) => {
+  if (n[id] === id)
+    return id
+
+  const resID = getRoot(n[id])
+  n[id] = resID
+
+  return resID
+}
+
+const checkConnection = (id1, id2) =>
+  getRoot(id1) === getRoot(id2)
+
+const unite = (id1, id2) => {
+  const rootID = getRoot(id1)
+
+  n[id2] = n[rootID]
+
+  !treesWeight[rootID] &&
+    (treesWeight[rootID] = 0)
+
+  ++treesWeight[rootID]
+}
+
+unite(0, 1)
+unite(1, 2)
+
+unite(9, 8)
+unite(8, 7)
+
+unite(2, 7)
+
+console.log(n)
+console.log(treesWeight)
